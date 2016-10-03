8c1925769272bfc33104474febeeb81c45cc362c
WHY-1W <694175346@qq.com>
2016-10-03 21:10:39 +0800

    add 斗技

--- scripts/base.lua
+++ scripts/base.lua
@@ -7,24 +7,25 @@ end
 
 -- 点击
 function click(x, y)
-    touchDown(0, x, y);
-    mSleep(500);
-    touchUp(0);
+    touchDown(0, x, y)
+    mSleep(500)
+    touchUp(0)
 end
 
 -- 点击准备按钮
 function clickReady()
     local p = {1909, 1268}
     while getColor(p[1], p[2]) ~= 0xc86D22 do
-        mSleep(500);
+        mSleep(500)
     end
-    click(p[1], p[2]); 
+    click(p[1], p[2])
 end
 
 -- 等到战斗结束
-function waitUntilBattleOver()
+function waitUntilBattleOver(handler)
     local p = {110, 1500}
-    while getColor(p[1], p[2]) == 0x836d54 do
-        mSleep(500);
+    while getColor(p[1], p[2]) == 0x836d54 or getColor(p[1], p[2]) == 0x665341 do
+        handler()
+        mSleep(1000)
     end
 end
---
+++ scripts/斗技.lua
@@ -0,0 +1,29 @@
+package.path=package.path .. ";/var/touchelf/scripts/?.lua"
+require "base"
+
+function clickAuto()
+   click(89, 1445)
+   mSleep(2000)
+   click(89, 1445)
+end
+
+function main()
+    setup()
+    
+    -- 点击 挑战 按钮
+    local p0 = {1808,1158}
+    click(p0[1], p0[2]);
+    
+    -- 点击 准备 按钮
+    clickReady()
+
+    -- 检查是否处于战斗状态
+    waitUntilBattleOver(clickAuto)
+    
+    -- 检查是否结束
+    while getColor(p0[1], p0[2]) ~= 0x272420 do
+        click(p0[1], p0[2])            
+        mSleep(500)
+    end
+
+end
